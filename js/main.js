6fb7e5aaa7a4cafaa824a6e848b4346c581efc0c
ziyamrzyvcode <ziyamirzeyev19@gmail.com>
2024-09-16 14:33:50 +0400

    Update

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ fetch('./data/menu.json')
 let dropDowns = `<div class="flex items-center">
                     <div class="text-left menu-item">
                         <div>
-                            <button type="button" onclick="handleMenuOpen('dropDownValyutaMob')" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            <button type="button" onclick="handleMenuOpen('dropDownValyutaMob')" class="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900" id="menu-button" aria-expanded="true" aria-haspopup="true">
                                 ₼
                                 <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </div>
-                        <div id="dropDownValyutaMob" class="absolute hidden z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                        <div id="dropDownValyutaMob" class="absolute hidden z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none drpItem" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                             <div class="py-1" role="none">
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">₼</a>
                                 <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="menu-item-0">$</a>
@@ -22,14 +22,14 @@ let dropDowns = `<div class="flex items-center">
                     </div>
                     <div>
                         <div>
-                            <button type="button" onclick="handleMenuOpen('dropDownDilMob')" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+                            <button type="button" onclick="handleMenuOpen('dropDownDilMob')" class="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 text-sm font-semibold text-gray-900" id="menu-button" aria-expanded="true" aria-haspopup="true">
                                 <img alt="az" loading="lazy" width="24" height="16" decoding="async" data-nimg="1" class="w-6 h-4 object-cover" src="https://sayt.az/images/az.png" style="color: transparent;">
                                 <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </div>
-                        <div id="dropDownDilMob" class="absolute hidden z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                        <div id="dropDownDilMob" class="absolute hidden z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none drpItem" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                             <div class="py-1" role="none">
                                 <div class="my-1"><img alt="az" loading="lazy" width="24" height="16" decoding="async" data-nimg="1" class="w-6 h-4 object-cover" src="https://sayt.az/images/az.png" style="color: transparent;"></div>
                                 <div class="my-1"><img alt="az" loading="lazy" width="24" height="16" decoding="async" data-nimg="1" class="w-6 h-4 object-cover" src="https://sayt.az/images/usa.png" style="color: transparent;"></div>   
@@ -72,7 +72,7 @@ function handleDisplayMenuItems(menuItems, menuId) {
                           </svg>
                       </button>
                   </div>
-                  <div id="${menuId}_${item.id}" class="absolute hidden z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
+                  <div id="${menuId}_${item.id}" class="absolute hidden z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none drpItem" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                       <div class="py-1" role="none">
                           ${subMenuTxt}
                       </div>
